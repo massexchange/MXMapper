32b72bc27cf4f73a38cb0f30d52ba4485d95b533
Eugene Dobry <eugene.dobry@massexchange.com>
2015-11-24 20:36:25 -0500

    formatting

--- coreMappings.js
+++ coreMappings.js
@@ -7,24 +7,37 @@ nconf.argv();
 var mpId = nconf.get("mp");
 
 var mappingsWithoutMp = [
-    { inputAttribute: { key: "Dimension.METRO_NAME", value: "*" }, outputAttributes: [
-            { key: "DMA", value: "*" }]},
-    { inputAttribute: { key: "Dimension.AD_UNIT_NAME", value: "*" }, outputAttributes: [
-            { key: "AdSize", value: "*" }]},
-    { inputAttribute: { key: "Dimension.DEVICE_CATEGORY_NAME", value: "*" }, outputAttributes: [
-            { key: "Device", value: "*" }]},
-    { inputAttribute: { key: "Dimension.COUNTRY_NAME", value: "*" }, outputAttributes: [
-            { key: "Nation", value: "*" }]},
-    { inputAttribute: { key: "MONTH", value: "*" }, outputAttributes: [
-            { key: "Flight Date", value: "*" }]},
-    { inputAttribute: { key: "Demo Title", value: "*" }, outputAttributes: [
-            { key: "Gender", value: "*" }]},
-    { inputAttribute: { key: "Demo Value Title", value: "*" }, outputAttributes: [
-            { key: "AgeRange", value: "*" }]},
-    { inputAttribute: { key: "Dimension.AD_UNIT_ID", value: "*" }, outputAttributes: [] },
-    { inputAttribute: { key: "Dimension.COUNTRY_CRITERIA_ID", value: "*" }, outputAttributes: [] },
-    { inputAttribute: { key: "Dimension.METRO_CRITERIA_ID", value: "*" }, outputAttributes: [] },
-    { inputAttribute: { key: "Dimension.DEVICE_CATEGORY_ID", value: "*" }, outputAttributes: [] }
+    {
+        inputAttribute: { key: "Dimension.METRO_NAME", value: "*" },
+        outputAttributes: [{ key: "DMA", value: "*" }]
+    }, {
+        inputAttribute: { key: "Dimension.AD_UNIT_NAME", value: "*" },
+        outputAttributes: [{ key: "AdSize", value: "*" }]
+    }, {
+        inputAttribute: { key: "Dimension.DEVICE_CATEGORY_NAME", value: "*" },
+        outputAttributes: [{ key: "Device", value: "*" }]
+    }, {
+        inputAttribute: { key: "Dimension.COUNTRY_NAME", value: "*" },
+        outputAttributes: [{ key: "Nation", value: "*" }]
+    }, {
+        inputAttribute: { key: "Demo Title", value: "*" },
+        outputAttributes: [{ key: "Gender", value: "*" }]
+    }, {
+        inputAttribute: { key: "Demo Value Title", value: "*" },
+        outputAttributes: [{ key: "AgeRange", value: "*" }]
+    }, {
+        inputAttribute: { key: "Dimension.AD_UNIT_ID", value: "*" },
+        outputAttributes: []
+    }, {
+        inputAttribute: { key: "Dimension.COUNTRY_CRITERIA_ID", value: "*" },
+        outputAttributes: []
+    }, {
+        inputAttribute: { key: "Dimension.METRO_CRITERIA_ID", value: "*" },
+        outputAttributes: []
+    }, {
+        inputAttribute: { key: "Dimension.DEVICE_CATEGORY_ID", value: "*" },
+        outputAttributes: []
+    }
 ];
 
 var finalMappings = mappingsWithoutMp.map(function(mapping) {
