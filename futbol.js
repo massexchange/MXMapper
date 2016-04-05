b4a12e66472081f420c225e6c5d2a601c03845d5
Eugene Dobry <eugene.dobry@massexchange.com>
2016-04-04 20:14:51 -0400

    rename, clean up

--- futbol.js
+++ futbol.js
@@ -1,53 +1,68 @@
-var transform = require("./transform"),
+var prepare = require("./prepare"),
     util = require("./util")
 
     nconf = require("nconf"),
     reduce = require("stream-reduce");
 
+var colName = "Dimension.AD_UNIT_NAME";
 var columns = ["Publication", "Section"];
 
-var parseName = (name) => util.zip(columns, name.split('/'))
-                            .filter(tuple => tuple.every(x => x))
-                            .reduce((out, tuple) => {
-                                out[tuple[0]] = tuple[1];
-                                return out;
-                            }, {});
+//main parsing logic
+var parseName = (name) =>
+    //create protoAttr kvps
+    util.zip(columns, name.split('/'))
+        //filter out value-less protoAttrs
+        .filter(tuple => tuple.every(x => x))
+        //convert tuples to map
+        .reduce((out, tuple) => {
+            out[tuple[0]] = tuple[1];
+            return out;
+        }, {});
 
-var colName = "Dimension.AD_UNIT_NAME";
 
-transform().then(({ source, sink }) => {
+//extract relevant column
+var columnExtractor = util.mapStream(raw =>
+    raw.attributes.filter(attr => attr.key == colName)[0].value
+);
+
+//create protoMappings
+var protoMapper = util.mapStream(colValue => ({
+    adUnitName: colValue,
+    values: parseName(colValue)
+}));
+
+//transform to real mappings
+//reduce to a map to filter duplicates
+var mappingReducer = reduce((mappings, proto) => {
+    if(!mappings[proto.adUnitName])
+        mappings[proto.adUnitName] = {
+            mp: { id: nconf.get("mp") },
+            inputAttribute: {
+                key: colName,
+                value: proto.adUnitName
+            },
+            outputAttributes:
+                //convert protoMappings
+                Object.keys(proto.values)
+                    .map(type => ({
+                        key: type,
+                        value: proto.values[type]
+                    }))
+        };
+
+    return mappings;
+}, {});
+
+var flatMapper = util.flatMapStream(mappings =>
+    Object.keys(mappings)
+        .reduce(util.flatMapReducer(key => mappings[key]), [])
+);
+
+prepare().then(({ source, sink }) => {
     source
-        .pipe(util.mapStream(raw =>
-            raw.attributes.filter(attr => attr.key == colName)[0].value
-        ))
-        //create protoMappings
-        .pipe(util.mapStream(colValue => ({
-            adUnitName: colValue,
-            values: parseName(colValue)
-        })))
-        //transform to real mappings
-        //reduce to a map to filter duplicates
-        .pipe(reduce((mappings, proto) => {
-            if(!mappings[proto.adUnitName])
-                mappings[proto.adUnitName] = {
-                    mp: { id: nconf.get("mp") },
-                    inputAttribute: {
-                        key: colName,
-                        value: proto.adUnitName
-                    },
-                    outputAttributes:
-                        Object.keys(proto.values)
-                            .map(type => ({
-                                key: type,
-                                value: proto.values[type]
-                            }))
-                };
-
-            return mappings;
-        }, {}))
-        .pipe(util.flatMapStream(mappings =>
-            Object.keys(mappings)
-                .reduce(util.flatMapReducer(key => mappings[key]), [])
-        ))
+        .pipe(columnExtractor)
+        .pipe(protoMapper)
+        .pipe(mappingReducer)
+        .pipe(flatMapper)
         .pipe(sink);
 });
